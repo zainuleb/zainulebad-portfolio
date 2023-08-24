b9f48a2413412f1add38dffdf8d17c392be8dee9
zainuleb <zainulebadd@gmail.com>
2023-08-25 03:16:50 +0500

    configuring tailwind

--- src/component/Mobile.js
+++ src/component/Mobile.js
@@ -3,114 +3,114 @@ import React from "react";
 const Mobile = () => {
   return (
     <>
-      <div class="elisc_tm_topbar fixed top-0 left-0 right-0 h-[50px] bg-white z-[15] hidden">
-        <div class="topbar_inner w-full h-full clear-both flex items-center justify-between py-0 px-[20px]">
-          <div class="logo" data-type="image">
-            <a class="image" href="#">
+      <div className="elisc_tm_topbar fixed top-0 left-0 right-0 h-[50px] bg-white z-[15] hidden">
+        <div className="topbar_inner w-full h-full clear-both flex items-center justify-between py-0 px-[20px]">
+          <div className="logo" data-type="image">
+            <a className="image" href="#/">
               <img
-                class="max-w-[100px] max-h-[40px]"
+                className="max-w-[100px] max-h-[40px]"
                 src="assets/img/logo/logo.png"
                 alt=""
               />
             </a>
-            <a class="text" href="#">
+            <a className="text" href="#/">
               <span>R.Elisc</span>
             </a>
           </div>
-          <div class="trigger">
-            <div class="hamburger hamburger--slider">
-              <div class="hamburger-box">
-                <div class="hamburger-inner"></div>
+          <div className="trigger">
+            <div className="hamburger hamburger--slider">
+              <div className="hamburger-box">
+                <div className="hamburger-inner"></div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="elisc_tm_mobile_menu">
-        <div class="inner relative w-full h-full text-right px-[20px] pt-[70px] pb-[20px]">
-          <div class="wrapper">
-            <div class="avatar w-[70px] h-[70px] relative float-right mb-[50px]">
+      <div className="elisc_tm_mobile_menu">
+        <div className="inner relative w-full h-full text-right px-[20px] pt-[70px] pb-[20px]">
+          <div className="wrapper">
+            <div className="avatar w-[70px] h-[70px] relative float-right mb-[50px]">
               <div
-                class="image absolute inset-0 bg-no-repeat bg-cover bg-center"
+                className="image absolute inset-0 bg-no-repeat bg-cover bg-center"
                 data-img-url="assets/img/about/1.jpg"
               ></div>
             </div>
-            <div class="menu_list w-full h-auto clear-both float-left mb-[50px]">
-              <ul class="transition_link m-0">
-                <li class="active mb-[7px]">
-                  <a class="text-dark-color" href="#home">
+            <div className="menu_list w-full h-auto clear-both float-left mb-[50px]">
+              <ul className="transition_link m-0">
+                <li className="active mb-[7px]">
+                  <a className="text-dark-color" href="#home">
                     Home
                   </a>
                 </li>
-                <li class="mb-[7px]">
-                  <a class="text-dark-color" href="#about">
+                <li className="mb-[7px]">
+                  <a className="text-dark-color" href="#about">
                     About
                   </a>
                 </li>
-                <li class="mb-[7px]">
-                  <a class="text-dark-color" href="#service">
+                <li className="mb-[7px]">
+                  <a className="text-dark-color" href="#service">
                     Services
                   </a>
                 </li>
-                <li class="mb-[7px]">
-                  <a class="text-dark-color" href="#portfolio">
+                <li className="mb-[7px]">
+                  <a className="text-dark-color" href="#portfolio">
                     Portfolio
                   </a>
                 </li>
-                <li class="mb-[7px]">
-                  <a class="text-dark-color" href="#news">
+                <li className="mb-[7px]">
+                  <a className="text-dark-color" href="#news">
                     Blog
                   </a>
                 </li>
-                <li class="mb-[7px]">
-                  <a class="text-dark-color" href="#contact">
+                <li className="mb-[7px]">
+                  <a className="text-dark-color" href="#contact">
                     Contact
                   </a>
                 </li>
               </ul>
             </div>
-            <div class="social w-full float-left mb-[5px]">
+            <div className="social w-full float-left mb-[5px]">
               <ul>
-                <li class="mr-[8px] inline-block">
-                  <a class="text-[#333]" href="#">
+                <li className="mr-[8px] inline-block">
+                  <a className="text-[#333]" href="#/">
                     <img
-                      class="svg"
+                      className="svg"
                       src="assets/img/svg/social/facebook.svg"
                       alt=""
                     />
                   </a>
                 </li>
-                <li class="mr-[8px] inline-block">
-                  <a class="text-[#333]" href="#">
+                <li className="mr-[8px] inline-block">
+                  <a className="text-[#333]" href="#/">
                     <img
-                      class="svg"
+                      className="svg"
                       src="assets/img/svg/social/twitter.svg"
                       alt=""
                     />
                   </a>
                 </li>
-                <li class="mr-[8px] inline-block">
-                  <a class="text-[#333]" href="#">
+                <li className="mr-[8px] inline-block">
+                  <a className="text-[#333]" href="#/">
                     <img
-                      class="svg"
+                      className="svg"
                       src="assets/img/svg/social/instagram.svg"
                       alt=""
                     />
                   </a>
                 </li>
-                <li class="mr-[8px] inline-block">
-                  <a class="text-[#333]" href="#">
+                <li className="mr-[8px] inline-block">
+                  <a className="text-[#333]" href="#/">
                     <img
-                      class="svg"
+                      className="svg"
                       src="assets/img/svg/social/dribbble.svg"
                       alt=""
                     />
                   </a>
                 </li>
-                <li class="inline-block">
-                  <a class="text-[#333]" href="#">
+                <li className="inline-block">
+                  <a className="text-[#333]" href="#/">
                     <img
-                      class="svg"
+                      className="svg"
                       src="assets/img/svg/social/tik-tok.svg"
                       alt=""
                     />
@@ -118,8 +118,8 @@ const Mobile = () => {
                 </li>
               </ul>
             </div>
-            <div class="copyright w-full float-left">
-              <p class="text-dark-color">Copyright &copy; 2023</p>
+            <div className="copyright w-full float-left">
+              <p className="text-dark-color">Copyright &copy; 2023</p>
             </div>
           </div>
         </div>
